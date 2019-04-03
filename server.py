f762e9ff3efa779d0d55ea977206f5dc573e6483
Debashis Das <Debashis.Das@walmartlabs.com>
2019-04-03 17:15:02 +0530

    Deb | Added Swagger UI

--- server.py
+++ server.py
@@ -2,10 +2,10 @@ from flask import render_template
 import connexion
 
 # Create the application instance
-app = connexion.App(__name__, specification_dir='./')
+application = connexion.App(__name__, specification_dir='./')
 
 # Read the swagger.yml file to configure the endpoints
-app.add_api('swagger.yml')
+application.add_api('swagger.yml')
 
 # Create a URL route in our application for "/"
 @app.route('/')
@@ -14,4 +14,4 @@ def home():
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    application.run(host='0.0.0.0')
