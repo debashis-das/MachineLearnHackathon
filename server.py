55f08779a0fa7c0a445fd73c5d38be831976fc31
Debashis Das <Debashis.Das@walmartlabs.com>
2019-04-05 01:41:56 +0530

    Deb | added left CROS dependencies

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 from flask import render_template
 import connexion
-
+from flask_cors import CORS, cross_origin
 # Create the application instance
 application = connexion.App(__name__, specification_dir='./')
 
 # Read the swagger.yml file to configure the endpoints
 application.add_api('swagger.yml')
 cors = CORS(application)
-app.config['CORS_HEADERS'] = 'Content-Type'
+application.config['CORS_HEADERS'] = 'Content-Type'
 # Create a URL route in our application for "/"
 @application.route('/')
 @cross_origin()
