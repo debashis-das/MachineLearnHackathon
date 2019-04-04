402ed6a7fb061d2c81ee7c6843c3734349e34ed7
Debashis Das <Debashis.Das@walmartlabs.com>
2019-04-05 01:37:35 +0530

    Deb | enabled CORS

--- server.py
+++ server.py
@@ -6,9 +6,11 @@ application = connexion.App(__name__, specification_dir='./')
 
 # Read the swagger.yml file to configure the endpoints
 application.add_api('swagger.yml')
-
+cors = CORS(application)
+app.config['CORS_HEADERS'] = 'Content-Type'
 # Create a URL route in our application for "/"
 @application.route('/')
+@cross_origin()
 def home():
     return render_template('home.html')
 
