0b166b3c1fef0798d74a5dbfde958185e0565638
Debashis Das <Debashis.Das@walmartlabs.com>
2019-04-05 01:54:00 +0530

    Deb | fixed connexion bug

--- server.py
+++ server.py
@@ -1,19 +1,22 @@
 from flask import render_template
 import connexion
-from flask_cors import CORS, cross_origin
-# Create the application instance
-application = connexion.App(__name__, specification_dir='./')
 
-# Read the swagger.yml file to configure the endpoints
+def set_cors_headers_on_response(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With'
+    response.headers['Access-Control-Allow-Methods'] = 'OPTIONS'
+    return response
+
+# Create the application instance
+application = connexion.FlaskApp(__name__, specification_dir='./')
 application.add_api('swagger.yml')
-cors = CORS(application)
-application.config['CORS_HEADERS'] = 'Content-Type'
+application.app.after_request(set_cors_headers_on_response)
 # Create a URL route in our application for "/"
 @application.route('/')
-@cross_origin()
 def home():
     return render_template('home.html')
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
     application.run()
+    
